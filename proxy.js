943e8dbac6ca8c3c1bcd6bf86922542bbc58fef6
kp1153 <kamta_tiwari@yahoo.com>
2026-02-25 15:22:43 +0530

    force deploy

--- proxy.js
+++ proxy.js
@@ -2,18 +2,17 @@ import { NextResponse } from "next/server";
 
 export function proxy(request) {
   const session = request.cookies.get("session");
-  const isAuth = !!session?.value;  // किसी भी value को authenticate मानो
+  console.log("SESSION:", session);
+  const isAuth = !!session?.value;
   const pathname = request.nextUrl.pathname;
   
   const publicPaths = ["/", "/login"];
   const isPublicPath = publicPaths.includes(pathname);
 
-  // अगर authenticate नहीं और public path नहीं तो login पर भेजो
   if (!isAuth && !isPublicPath) {
     return NextResponse.redirect(new URL("/login", request.url));
   }
 
-  // अगर authenticate है और login page पर है तो dashboard पर भेजो
   if (isAuth && pathname === "/login") {
     return NextResponse.redirect(new URL("/dashboard", request.url));
   }
